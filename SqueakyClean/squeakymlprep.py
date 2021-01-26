04fb8a27284cc6b4c89b1188d70301f0954040a9
VincentChung <48785236+Vincent-Chung@users.noreply.github.com>
2021-01-26 17:45:11 -0600

    Add OneHotEncode function

--- SqueakyClean/squeakymlprep.py
+++ SqueakyClean/squeakymlprep.py
@@ -1,1 +1,10 @@
 # Functions to help with preparing dataset for ML
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def OneHotEncode(df, Col):
+    temp_df = pd.get_dummies(df[Col],drop_first = True)
+    df = df.merge(temp_df, left_index = True, right_index = True)
+    df = df.drop([Col], axis = 1)
+    return df
